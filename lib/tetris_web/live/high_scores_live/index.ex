34327feeb8651e22deeaf691ec30c1397f6c48e9
Marc <marc@schopper.rocks>
2025-09-14 11:43:31 +0200

    fix(scores): limit /scores page to top 10 entries

--- lib/tetris_web/live/high_scores_live/index.ex
+++ lib/tetris_web/live/high_scores_live/index.ex
@@ -3,7 +3,6 @@ defmodule TetrisWeb.HighScoresLive.Index do
   alias Tetris.HighScores
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, top_scores: HighScores.top(50))}
+    {:ok, assign(socket, top_scores: HighScores.top(10))}
   end
 end
-
